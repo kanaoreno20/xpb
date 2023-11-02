afc25ff4ff9592415e14e14a8a97b892c688e6e7
kanaoreno20 <r202301969kb@jindai.jp>
2023-11-02 19:17:20 +0900

    202301101

---
+++ de12/python/rand.py
@@ -0,0 +1,52 @@
+correct=90
+wrong=90
+
+print("ワンピースクイズ")
+
+a=input("黒足の？")
+
+if a == "サンジ":
+ print("正解")
+correct=correct+0
+
+else:
+print("不正解")
+wrong=wrong-30
+
+
+b = input("フランキーの師匠・トムの笑い方は？")
+1=input("まーはっはっは")
+2=input("ガッハッハ")
+3=input("たっはっは")
+if b == 3
+print("正解")
+correct=correct+0
+else:
+    print("不正解")
+    wrong=wrong-30
+
+c = input("「弱ェ奴は死に方も選べねェ」というセリフを言っていないのは誰？")
+1=input("ドンキホーテ・ドフラミンゴ")
+2=input("ロロノア・ゾロ")
+3=input("トラファルガー・ロー")
+
+if c == 2
+print("正解")
+correct=correct+0
+else:
+    print("不正解")
+    wrong=wrong-30
+
+
+print("おわり")
+if correct=90
+print("あなたはワンピースマニアです")
+
+if correct=60
+print("素晴らしい")
+
+if correct=30
+print("一話から見直そう")
+
+if correct=0
+print("ワンピース見たことある？")
